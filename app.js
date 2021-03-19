c2cd514b1836380a245abd999fd2619004433af9
Chiahsuan Chou <belinda05021992@gmail.com>
2021-03-19 10:29:04 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 //import packages
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const port = 3000
+
+//setting connection
+mongoose.connect('mongodb://localhost/todo-list',  { useNewUrlParser: true,useUnifiedTopology: true  })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error',()=>{
+    console.log('mongodb error!')
+})
+// 連線成功
+db.once('open',()=>{
+    console.log('mongodb connected!')
+})
 
 //setting routers
 app.get('/',(req,res)=>{
