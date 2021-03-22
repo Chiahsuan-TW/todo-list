684ea0cd9bf9eaca478c66cc0f4c45e902407d50
Chiahsuan Chou <belinda05021992@gmail.com>
2021-03-22 13:32:42 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const port = 3000
 const exphbs = require('express-handlebars')
-const Todo = require('./models/todo')
+const routes = require('./routes')
 
 //setting connection
 mongoose.connect('mongodb://localhost/todo-list',  { useNewUrlParser: true,useUnifiedTopology: true  })
@@ -30,62 +30,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(methodOverride('_method'))
 
-//setting routers
-app.get('/',(req,res)=>{
-    Todo.find()
-    .lean()
-    .sort({ _id: 'asc'})
-    .then( todos => res.render('index', {todos}) )
-    .catch( error =>console.log(error))
-})
-
-app.get('/todos/new',(req,res)=>{
-    res.render('new')
-})
-
-app.post('/todos', (req,res)=>{
-    const name = req.body.name
-    return Todo.create({name})
-     .then(() => res.redirect('/'))
-     .catch(error => console.log(error) )
-})
-
-app.get('/todos/:id',(req,res)=>{
-    const id =req.params.id
-    return Todo.findById(id)
-    .lean()
-    .then( todo => res.render('detail', {todo}))
-    .catch(error => console.log(error) )
-})
-
-app.get('/todos/:id/edit',(req,res)=>{
-    const id =req.params.id
-    return Todo.findById(id)
-    .lean()
-    .then( todo => res.render('edit', {todo}))
-    .catch(error => console.log(error) )
-})
-
-app.put('/todos/:id',(req,res)=>{
-    const {name, isDone} = req.body
-    const id = req.params.id
-    return Todo.findById(id)
-    .then( todo =>{
-        todo.name = name
-        todo.isDone = isDone === 'on'
-        return todo.save() 
-    })
-    .then(()=> res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error) )
-})
-
-app.delete('/todos/:id', (req,res)=>{
-    const id = req.params.id
-    return Todo.findById(id)
-     .then( todo => todo.remove())
-     .then(()=> res.redirect('/'))
-     .catch(error => console.log(error) )
-})
+app.use(routes)
 
 
 //start and listen the server
